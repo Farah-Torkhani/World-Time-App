a6c6c8e187439e742240e08de5ea20355bc4b4ec
Ranim-Rabah <79060004+Farah-Torkhani@users.noreply.github.com>
2023-07-23 13:13:51 +0100

    making a dynamic time changing

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../services/world_time.Dart';
+
 class ChooseLocation extends StatefulWidget {
   @override
   _ChooseLocationState createState() => _ChooseLocationState();
@@ -16,6 +17,17 @@ class _ChooseLocationState extends State<ChooseLocation> {
     WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
     WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
   ];
+  void updateTime(index) async {
+    WorldTime instance = locations[index];
+    await instance.getTime();
+    // navigate_to_home_screen
+    Navigator.pop(context, {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime': instance.isDaytime,
+    });
+  }
 
   @override
   void initState() {
@@ -40,7 +52,9 @@ class _ChooseLocationState extends State<ChooseLocation> {
             padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
             child: Card(
               child: ListTile(
-                onTap: () {},
+                onTap: () {
+                  updateTime(index);
+                },
                 title: Text(locations[index].location),
                 leading: CircleAvatar(
                   backgroundImage:
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,93 @@
+// import 'package:flutter/material.dart';
+// import 'dart:convert';
+
+// class Home extends StatefulWidget {
+//   @override
+//   _HomeState createState() => _HomeState();
+// }
+
+// class _HomeState extends State<Home> {
+//   Map data = {};
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // Retrieve the arguments from ModalRoute
+// dynamic arguments = ModalRoute.of(context)?.settings.arguments;
+
+//     if (arguments != null && arguments is Map) {
+//       data = arguments;
+//     } else {
+//       // Handle the case when arguments are null or not of the expected type
+//       data = {
+//         'location': 'Unknown Location',
+//         'time': 'Unknown Time',
+//         'isDaytime': true,
+//       };
+
+//     }
+//     return Scaffold(
+//         backgroundColor: bgColor,
+//         body: SafeArea(
+//       child: Container(
+//         decoration: BoxDecoration(
+//           image: DecorationImage(
+//             image: AssetImage('assets/$bgImage'),
+//             fit: BoxFit.cover,
+//           ),
+//         ),
+//         child: Padding(
+//           padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+//           child: Column(
+//             children: <Widget>[
+//               TextButton.icon(
+//                     onPressed: () async {
+//                       dynamic result =
+//                           await Navigator.pushNamed(context, '/location');
+//                       setState(() {
+//                         data = {
+//                           'time': result['time'],
+//                           'location': result['location'],
+//                           'isDaytime': result['isDaytime'],
+//                           'flag': result['flag']
+//                         };
+//                       });
+//                 },
+//                     icon: Icon(Icons.edit_location, color: Colors.grey[300]),
+
+//                 label: Text('Edit Location'),
+//               ),
+//               SizedBox(height: 20.0),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: <Widget>[
+//                   Text(
+//                     data['location'],
+//                     style: TextStyle(
+//                       fontSize: 28.0,
+//                       letterSpacing: 2.0,
+//                           color: Colors.white,
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//               SizedBox(height: 20.0),
+//               Text(
+//                 data['time'],
+//                 style: TextStyle(
+//                   fontSize: 66.0,
+//                       color: Colors.white
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+// )
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'dart:convert';
 
 class Home extends StatefulWidget {
   @override
@@ -9,75 +97,84 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   Map data = {};
 
+//  @override
+//  void initState() {
+//    super.initState();
+//  }
+
   @override
   Widget build(BuildContext context) {
-    // Retrieve the arguments from ModalRoute
-    dynamic arguments = ModalRoute.of(context)!.settings.arguments;
+data = data.isNotEmpty
+        ? data
+        : ModalRoute.of(context)?.settings.arguments as Map<dynamic, dynamic>;
 
-    if (arguments != null && arguments is Map) {
-      data = arguments;
-    } else {
-      // Handle the case when arguments are null or not of the expected type
-      data = {
-        'location': 'Unknown Location',
-        'time': 'Unknown Time',
-        'isDaytime': true,
-      };
-    
-    }
+
+    // set background image
 String bgImage = (data['isDaytime'] ?? false) ? 'day.png' : 'night.png';
-Color bgColor =
+    Color bgColor =
         (data['isDaytime'] ?? false) ? Colors.blue : Colors.indigo[700]!;
 
     return Scaffold(
-        backgroundColor: bgColor,
-        body: SafeArea(
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
+      backgroundColor: bgColor,
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
             image: AssetImage('assets/$bgImage'),
             fit: BoxFit.cover,
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
-          child: Column(
-            children: <Widget>[
-              TextButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
-                },
-                    icon: Icon(Icons.edit_location, color: Colors.grey[300]),
-               
-                label: Text('Edit Location'),
-              ),
-              SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    data['location'],
+          )),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+            child: Column(
+              children: <Widget>[
+                TextButton.icon(
+                  onPressed: () async {
+                    dynamic result =
+                        await Navigator.pushNamed(context, '/location');
+                    if (result != null) {
+                      setState(() {
+                        data = {
+                          'time': result['time'],
+                          'location': result['location'],
+                          'isDaytime': result['isDaytime'],
+                          'flag': result['flag']
+                        };
+                      });
+                    }
+                  },
+                  icon: Icon(
+                    Icons.edit_location,
+                    color: Colors.grey[300],
+                  ),
+                  label: Text(
+                    'Edit Location',
                     style: TextStyle(
-                      fontSize: 28.0,
-                      letterSpacing: 2.0,
-                          color: Colors.white,
+                      color: Colors.grey[300],
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                data['time'],
-                style: TextStyle(
-                  fontSize: 66.0,
-                      color: Colors.white
                 ),
-              ),
-            ],
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Text(data['time'],
+                    style: TextStyle(fontSize: 66.0, color: Colors.white)),
+              ],
+            ),
           ),
         ),
       ),
-)
     );
   }
 }
